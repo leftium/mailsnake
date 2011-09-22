400e5aeca8f817d031453512aff68483cd1f43b1
Hassek <nullwriter@gmail.com>
2011-09-22 14:46:15 -0430

    added logic to support the STS mailchimp api and recursive urlencoding for utf-8

--- mailsnake/mailsnake.py
+++ mailsnake/mailsnake.py
@@ -1,10 +1,15 @@
 import urllib2
+import urllib
 try:
     import simplejson as json
 except ImportError:
     import json
 
 class MailSnake(object):
+
+    dc = 'us1'
+    base_api_url = 'https://%(dc)s.api.mailchimp.com/1.3/?method=%(method)s'
+
     def __init__(self, apikey = '', extra_params = {}):
         """
             Cache API key and address.
@@ -14,17 +19,15 @@ class MailSnake(object):
         self.default_params = {'apikey':apikey}
         self.default_params.update(extra_params)
 
-        dc = 'us1'
         if '-' in self.apikey:
-            dc = self.apikey.split('-')[1]
-        self.base_api_url = 'https://%s.api.mailchimp.com/1.3/?method=' % dc
+            self.dc = self.apikey.split('-')[1]
 
     def call(self, method, params = {}):
-        url = self.base_api_url + method
+        url = self.base_api_url%{'dc': self.dc, 'method': method}
         params.update(self.default_params)
 
         post_data = urllib2.quote(json.dumps(params))
-        headers = {'Content-Type': 'application/json'}
+        headers={'Content-Type': 'application/json'}
         request = urllib2.Request(url, post_data, headers)
         response = urllib2.urlopen(request)
 
@@ -38,4 +41,28 @@ class MailSnake(object):
             return self.call(method_name, params)
 
         return get.__get__(self)
+
+class MailSnakeSTS(MailSnake):
+    base_api_url = 'http://%(dc)s.sts.mailchimp.com/1.0/%(method)s'
+
+    def _url_encode(self, data):
+        def _(value):
+            try:
+                val = str(value).encode('utf-8')
+            except:
+                val = value.encode('utf-8')
+
+            return urllib.quote(val)
+
+        return '&'.join([ v for val in [[ "%s[%s]=%s"%(k,ik, _(iv)) for ik, iv in v.items()] if type(v)==dict else ["%s=%s"%(k,_(v))] for k,v in data.items() ] for v in val ])
+
+    def call(self, method, params={}, headers = {}):
+        url = self.base_api_url%{'dc': self.dc, 'method': method}
+        params.update(self.default_params)
+
+        post_data = self._url_encode(params)
+        request = urllib2.Request(url, post_data, headers)
+        response = urllib2.urlopen(request)
+
+        return json.loads(response.read())
 
