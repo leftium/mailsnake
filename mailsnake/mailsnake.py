5f619c0200551f52a8d4067c10d34c7a1dc611e7
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2011-11-01 11:02:20 +0100

    Swapping urllib2 with requests
    We need less lines for the same things and everything is neater.

--- mailsnake/mailsnake.py
+++ mailsnake/mailsnake.py
@@ -1,17 +1,18 @@
-import urllib2
+import requests
 try:
     import simplejson as json
 except ImportError:
     import json
 
+
 class MailSnake(object):
-    def __init__(self, apikey = '', extra_params = {}):
+    def __init__(self, apikey='', extra_params={}):
         """
-            Cache API key and address.
+        Cache API key and address.
         """
         self.apikey = apikey
 
-        self.default_params = {'apikey':apikey}
+        self.default_params = {'apikey': apikey}
         self.default_params.update(extra_params)
 
         dc = 'us1'
@@ -19,23 +20,14 @@ class MailSnake(object):
             dc = self.apikey.split('-')[1]
         self.base_api_url = 'https://%s.api.mailchimp.com/1.3/?method=' % dc
 
-    def call(self, method, params = {}):
-        url = self.base_api_url + method
-        params.update(self.default_params)
-
-        post_data = urllib2.quote(json.dumps(params))
-        headers = {'Content-Type': 'application/json'}
-        request = urllib2.Request(url, post_data, headers)
-        response = urllib2.urlopen(request)
-
-        return json.loads(response.read())
+    def call(self, method, params):
+        return json.loads(requests.post(self.base_api_url + method, params).content)
 
     def __getattr__(self, method_name):
 
         def get(self, *args, **kwargs):
             params = dict((i,j) for (i,j) in enumerate(args))
-            params.update(kwargs)
+            params.update(kwargs.items() + self.default_params.items())
             return self.call(method_name, params)
 
         return get.__get__(self)
-
