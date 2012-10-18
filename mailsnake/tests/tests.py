67bb4ad23ed11248c123be095435d9a088fc0393
Brad Pitcher <bradpitcher@gmail.com>
2012-10-17 17:21:20 -0700

    add campaign related testing

--- mailsnake/tests/tests.py
+++ mailsnake/tests/tests.py
@@ -2,6 +2,8 @@ import unittest
 
 from collections import MutableSequence
 from mailsnake import MailSnake
+from random import random
+
 from .secret_keys import MAILCHIMP_API_KEY, MAILCHIMP_LIST_ID
 
 """
@@ -36,6 +38,35 @@ class TestMailChimpAPI(unittest.TestCase):
             assert 'message' in chimp_chatter[0].keys()
             assert chimp_chatter[0]['url'].find('mailchimp') > -1
 
+    # Campaign Related Methods
+
+    def test_campaignCreateDelete(self):
+        template_id = self._add_test_template()
+        from_email = self.mcapi.getAccountDetails()['contact']['email']
+        options = {
+            'list_id': MAILCHIMP_LIST_ID, 'subject': 'testing',
+            'from_email': from_email, 'from_name': 'Test From',
+            'to_name': 'Test To', 'template_id': template_id,
+            'inline_css': True, 'generate_text': True, 'title': 'testing'
+        }
+        test_content = '%f' % random()
+        content = {'html_std_content00': test_content}
+        campaign_id = self.mcapi.campaignCreate(
+            type='regular', options=options, content=content)
+        assert isinstance(campaign_id, unicode)
+        campaign_content = self.mcapi.campaignContent(cid=campaign_id)
+        assert 'html' in campaign_content
+        assert 'text' in campaign_content
+        assert test_content in campaign_content['html']
+        assert test_content in campaign_content['text']
+        default_content = 'default content'
+        assert not default_content in campaign_content['html']
+        assert not default_content in campaign_content['text']
+
+        # Clean up
+        assert self.mcapi.campaignDelete(cid=campaign_id)
+        assert self.mcapi.templateDel(id=template_id)
+
     # List Related Methods
 
     def test_lists(self):
@@ -57,15 +88,18 @@ class TestMailChimpAPI(unittest.TestCase):
             assert t_type in self.mcapi.templates(types=new_types)
 
     def test_templateAddDel(self):
+        template_id = self._add_test_template()
+        assert isinstance(template_id, int)
+        assert self.mcapi.templateDel(id=template_id)
+
+    def _add_test_template(self):
+        html = open('mailsnake/tests/template.html', 'r').read()
         templates = self.mcapi.templates(inactives={'include': True})
         template_names = [t['name'] for t in templates['user']]
-        html = open('mailsnake/tests/template.html', 'r').read()
         index = 0
         base_name = 'mailsnake_test_template'
         template_name = '%s%i' % (base_name, index)
         while template_name in template_names:
             index += 1
             template_name = '%s%i' % (base_name, index)
-        template_id = self.mcapi.templateAdd(name=template_name, html=html)
-        assert isinstance(template_id, int)
-        assert self.mcapi.templateDel(id=template_id)
+        return self.mcapi.templateAdd(name=template_name, html=html)
