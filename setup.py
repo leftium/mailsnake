0edd063618956cffd76b3d4d247b267967d54f94
Brad Pitcher <bradpitcher@gmail.com>
2015-03-31 07:57:29 -0700

    more trove classifiers, fixes #27

--- setup.py
+++ setup.py
@@ -31,6 +31,8 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
